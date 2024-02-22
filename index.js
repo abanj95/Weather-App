e8fa0c792eb74e1b66faa881c5a366b9887dd738
Dezeus <dezeus@De-zeuss-MacBook-Pro.local>
2024-02-21 22:04:46 -0600

    added function to call the city to front-end

--- index.js
+++ index.js
@@ -7,16 +7,31 @@ import 'dotenv/config';
 const app = express();
 const port = 3000;
 
-// weather API key
+// weather API key from my .env file. you can generate yours by reading openweathermap.org
 const API_KEY = process.env.API_KEY;
+// const API_URL = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=';
 
-//parsing the body
+//// for parsing application/x-www-form-urlencoded
 app.use(bodyparser.urlencoded({extended:true}));
 
 app.use(express.static('public'));
 
 app.get('/', (req, res)=>{
-    res.render('index.ejs');
+    res.render('index.ejs',{content: 'City will be shown here'});
+});
+
+app.post('/getWeather', async (req,res) => {
+    const cityId = req.body.city;
+    try{
+    const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=${API_KEY}`);
+    const content = JSON.stringify(response.data.city.name);
+    console.log("log here: "+content);
+    res.render('index.ejs',{content:content});
+    }
+    catch(error){
+
+        res.render('index.ejs',{ content: JSON.stringify(error.response)});
+    }
 });
 
 
