15534dcf21a01fb619e7372d7affa67145f47760
Dezeus <dezeus@De-zeuss-MBP.lan>
2024-02-26 01:16:42 -0600

    added cloud types & function

--- index.js
+++ index.js
@@ -17,36 +17,68 @@ app.use(bodyparser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
 app.get('/', (req, res)=>{
-    res.render('index.ejs',{content: 'City will be shown here'});
+    res.render('index.ejs',{content: 'City will be shown here', country: null, weather: null, feels: null, description: null});
 });
 
-// app.post('/getWeather', async (req,res) => {
-//     const cityId = req.body.city;
-//     try{
-//     const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=${API_KEY}`);
-//     const content = JSON.stringify(response.data.city.name);
-//     console.log("log here: "+content);
-//     res.render('index.ejs',{content:content});
-//     }
-//     catch(error){
-
-//         res.render('index.ejs',{ content: JSON.stringify(error.response)});
-//     }
-// });
 app.post('/getWeather',async(req,res)=>{
     const city = req.body.city;
     try{
     const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
     const content = response.data;
-    // console.log("log here: "+content.name+" visibility: "+content.visibility);
-    res.render('index.ejs',{content: content});
+    
+    const country = content.sys.country;
+    const feels = content.main.feels_like;
+    const description = content.weather[0].description;
+    // console.log("log here: "+content.name+" | visibility: "+content.visibility+" | country:"+content.sys.country+" | description: "+description+" | feel like: "+feels);
+    const weather = updateWeatherImage(description);
+    
+    res.render('index.ejs',{content: content.name, country: country, weather:weather,feels: feels, description: description});
     }
     catch(error){
-        res.render('index.ejs',{content: JSON.stringify(error.response)});
+        res.render('index.ejs',{content: JSON.stringify(error.response), country: null, weather: null, feels:null, description: null});
     }
 })
 
 
+// weather image handler funtion
+function updateWeatherImage(weatherDescription) {
+    let imageName;
+    
+    switch(weatherDescription.toLowerCase()) {
+        case 'clear sky':
+            imageName = 'clearsky.svg';
+            break;
+        case 'few clouds':
+            imageName = 'fewcloudy.svg';
+            break;
+        case 'scattered clouds':
+            imageName = 'scatteredclouds.svg';
+            break;
+        case 'broken clouds':
+            imageName = 'brokenclouds.svg';
+            break;
+        case 'shower rain':
+            imageName = 'showerrain.svg';
+            break;
+        case 'rain':
+            imageName = 'rain.svg';
+            break;
+        case 'thunderstorm':
+            imageName = 'thunderstorm.svg';
+            break;
+        case 'snow':
+            imageName = 'snow.svg';
+            break;
+        case 'mist':
+            imageName = 'mist.svg';
+            break;
+        
+        default:
+            imageName = 'default.svg'; // a default image when the weather description is not recognized
+    }
+
+    return imageName;
+}
 
 
 
